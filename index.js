48cd2095a5b5ad72b9fe00d16466c3fe5c93b68a
Ondrej Bartas <ondrej@bartas.cz>
2016-02-17 09:00:53 +0100

    Add api methods

--- index.js
+++ index.js
@@ -13,8 +13,26 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/api/v1/application/constraints', function (req, res) {
+  res.json({
+    amountInterval:          {min: 10, max: 2000, step: 10, defaultValue: 400},  // IntervalBean:BigDecimal    Scrollable Amount interval
+    termInterval:            {min: 3, max: 30, step: 1, defaultValue: 15},       // IntervalBean:Integer       Scrollable term interval
+  })
+})
+
+app.get('/api/v1/application/first-loan-offer', function (req, res) {
+  var amount = req.query.amount;
+  var term = req.query.term;
+
+  res.json({
+    totalPrincipal: amount,
+    term: term,
+    totalCostOfCredit: amount / 10,
+    totalRepayableAmount: amount * 1.2,
+    monthlyPayment: amount * 1.2 / term
+  })
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
